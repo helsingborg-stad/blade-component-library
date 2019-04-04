038a025382402fcc5f5b2becc6f95c4dd874a5a0
Sebastian Thulin <sebastian.thulin@helsingborg.se>
2019-04-04 15:30:44 +0200

    Reformatted

--- src/View/card.blade.php
+++ src/View/card.blade.php
@@ -4,11 +4,8 @@
         @if($title)
         <h4 class="c-card__title">{{$title}}</h4>
         @endif
-        
         @if($content)
         <p class="c-card__text">{{$content}}</p>
         @endif
-        <a href="#" class="c-card__link">Card link</a>
-        <a href="#" class="c-card__link">Card link</a>
     </div>
 </div>
