5b8802799dbaba4b3dff22df917f81747202536e
Sebastian Thulin <sebastian.thulin@helsingborg.se>
2019-04-10 15:58:15 +0200

    Gallery adjustment

--- src/Component/Card/card.blade.php
+++ src/Component/Card/card.blade.php
@@ -92,11 +92,11 @@
         @component_gallery([
             'list' => [
                 ['largeImage' => "https://picsum.photos/900/600?image=1026", 'smallImage' => "https://picsum.photos/300/200?image=1026", 'caption' => "Image with stuff", 'alt' => "The alt text"],
-                ['largeImage' => "https://picsum.photos/900/600?image=1026", 'smallImage' => "https://picsum.photos/300/200?image=1026", 'caption' => "Image with stuff", 'alt' => "The alt text"],
-                ['largeImage' => "https://picsum.photos/900/600?image=1026", 'smallImage' => "https://picsum.photos/300/200?image=1026", 'caption' => "Image with stuff", 'alt' => "The alt text"],
-                ['largeImage' => "https://picsum.photos/900/600?image=1026", 'smallImage' => "https://picsum.photos/300/200?image=1026", 'caption' => "Image with stuff", 'alt' => "The alt text"],
-                ['largeImage' => "https://picsum.photos/900/600?image=1026", 'smallImage' => "https://picsum.photos/300/200?image=1026", 'caption' => "Image with stuff", 'alt' => "The alt text"],
-                ['largeImage' => "https://picsum.photos/900/600?image=1026", 'smallImage' => "https://picsum.photos/300/200?image=1026", 'caption' => "Image with stuff", 'alt' => "The alt text"],
+                ['largeImage' => "https://picsum.photos/900/600?image=1038", 'smallImage' => "https://picsum.photos/300/200?image=1038", 'caption' => "Image with stuff", 'alt' => "The alt text"],
+                ['largeImage' => "https://picsum.photos/900/600?image=1043", 'smallImage' => "https://picsum.photos/300/200?image=1043", 'caption' => "Image with stuff", 'alt' => "The alt text"],
+                ['largeImage' => "https://picsum.photos/900/600?image=1039", 'smallImage' => "https://picsum.photos/300/200?image=1039", 'caption' => "Image with stuff", 'alt' => "The alt text"],
+                ['largeImage' => "https://picsum.photos/900/600?image=1006", 'smallImage' => "https://picsum.photos/300/200?image=1006", 'caption' => "Image with stuff", 'alt' => "The alt text"],
+                ['largeImage' => "https://picsum.photos/900/600?image=993", 'smallImage' => "https://picsum.photos/300/200?image=993", 'caption' => "Image with stuff", 'alt' => "The alt text"],
             ]
         ])
 
--- src/Component/Gallery/gallery.blade.php
+++ src/Component/Gallery/gallery.blade.php
@@ -2,15 +2,15 @@
 @if($list)
   <section class="{{ $class }}">
     @foreach($list as $item)
-    <li class="item-{{ $loop->index }}">
-      <a href="{{$item['largeImage']}}">
-        @image([
-            'src'=> $item['smallImage'],
-            'alt' => $item['alt'],
-            'caption' => $item['caption']
-        ])
-      </a>
-    </li>
+      <li class="item-{{ $loop->index }}">
+        <a href="{{$item['largeImage']}}">
+          @image([
+              'src'=> $item['smallImage'],
+              'alt' => $item['alt'],
+              'caption' => $item['caption']
+          ])
+        </a>
+      </li>
     @endforeach
   </section>
 @else
