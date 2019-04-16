8233082e6ecdcc85fffa9497d9a2e85b1a1fabde
Sebastian Thulin <sebastian.thulin@helsingborg.se>
2019-04-16 11:01:11 +0200

    Card fixes

--- src/Component/Card/card.blade.php
+++ src/Component/Card/card.blade.php
@@ -12,30 +12,33 @@
         <img class="{{$baseClass}}__image" src="{{$image}}" alt="{{$alt}}">
         @endif
     @endif
-    <div class="{{$baseClass}}__body">
 
-        {{ $slot }}
+    {{ $slot }}
 
-        @if($title)
-            @if($href) 
-                <a class="{{$baseClass}}__link" href="{{$href}}">
-                    <h4 class="{{$baseClass}}__title">{{$title}}</h4>
-                </a>
-            @else
-            <h4 class="{{$baseClass}}__title">{{$title}}</h4>
+    @if($title != ""||$content != "")
+        <div class="{{$baseClass}}__body">
+
+            @if($title)
+                @if($href) 
+                    <a class="{{$baseClass}}__link" href="{{$href}}">
+                        <h4 class="{{$baseClass}}__title">{{$title}}</h4>
+                    </a>
+                @else
+                <h4 class="{{$baseClass}}__title">{{$title}}</h4>
+                @endif
             @endif
-        @endif
 
-        @if($content)
-            @if($href) 
-                <a class="{{$baseClass}}__link" href="{{$href}}">
-                    <p class="{{$baseClass}}__text">{{$content}}</p>
-                </a>
-            @else
-            <p class="{{$baseClass}}__text">{{$content}}</p>
+            @if($content)
+                @if($href) 
+                    <a class="{{$baseClass}}__link" href="{{$href}}">
+                        <p class="{{$baseClass}}__text">{{$content}}</p>
+                    </a>
+                @else
+                <p class="{{$baseClass}}__text">{{$content}}</p>
+                @endif
             @endif
-        @endif
-    </div>
+        </div>
+    @endif
 
     {{ $bottom }}
 
