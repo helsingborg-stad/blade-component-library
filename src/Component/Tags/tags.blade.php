313ea2ab28e5dcc0438453902cb163a0a654bfee
Jonatan Hanson <jonatan.hanson@gmail.com>
2019-04-17 16:00:17 +0200

    Added classlist as parameter to link and tags

--- src/Component/Tags/tags.blade.php
+++ src/Component/Tags/tags.blade.php
@@ -1,7 +1,7 @@
 @if($tags)
     <{{ $componentElement }} class="{{ $class }}">
         @foreach($tags as $tag)
-            @link(['href' => $tag['href'], 'class' => 'c-link--tag' ])
+            @link(['href' => $tag['href'], 'classList' => array('c-link--tag') ])
                 {{ $beforeLabel }}{{ $tag['label'] ?? 'Undefined label' }}{{ $afterLabel }}
             @endlink
         @endforeach
