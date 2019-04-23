695bb0d067e8e3bb444c6c5f08806e61e731e1b5
Jonatan Hanson <jonatan.hanson@gmail.com>
2019-04-23 15:29:31 +0200

    Tabs component

---
+++ src/Component/Tabs/tabs.blade.php
@@ -0,0 +1,21 @@
+<!-- tabs.blade.php -->
+@if($tabs)
+    <{{$componentElement}} class="{{ $class }}">
+        <div class="nav nav--{{$baseClass}}">
+            @foreach($tabs as $tab)
+                <div class="nav__item" data-tab="tab-{{$loop->index}}">
+                    <a class="nav__link" href="#" role="tab" tabindex="0" aria-selected="false">
+                        {{ $tab['title'] }}
+                    </a>
+                </div>
+            @endforeach
+        </div>
+        <div class="{{$baseClass}}__content">
+            @foreach($tabs as $tab)
+                <div data-tab="tab-{{$loop->index}}">
+                    {{ $tab['content'] }}
+                </div>
+            @endforeach
+        </div>
+    </{{$componentElement}}>
+@endif
