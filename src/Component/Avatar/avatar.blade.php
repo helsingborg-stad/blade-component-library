20486a42c5087cbcb687254db15ed0d3bc1d1c68
Sebastian Thulin <sebastian.thulin@helsingborg.se>
2019-05-07 10:33:16 +0200

    Render avatar initials as svg
    Makes it possible to render apropriate size to container.

--- src/Component/Avatar/avatar.blade.php
+++ src/Component/Avatar/avatar.blade.php
@@ -12,8 +12,8 @@
     @endif
 
     @if($initials)
-        <span class="{{$baseClass}}__initials" aria-label="{{ $label }}">
-            {{$initials}}
-        </span>
+        <svg class="{{$baseClass}}__initials" aria-label="{{ $label }}" viewBox="0 0 1000 1000" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+            <text font-size="300" y="50%" x="50%" fill="#fff" text-anchor="middle" alignment-baseline="central">{{$initials}}</text>
+        </svg>
     @endif
 </div>
