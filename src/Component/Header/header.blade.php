40086f1b5f4118962b6e3a865f4fc1f3b5848f3b
Sebastian Thulin <sebastian.thulin@helsingborg.se>
2019-05-09 11:32:21 +0200

    Horizontal menu

--- src/Component/Header/header.blade.php
+++ src/Component/Header/header.blade.php
@@ -4,16 +4,20 @@
   @if($includeContainer)
     <div class="container">
   @endif
-    <div class="grid">
-      <div class="grid-xs-3">
-        <a href="{{$logotypeHref}}">
-          {{$logotype}}
-        </a>
-      </div>
-      <div class="grid-xs-9">
-        {{$menu}}
+
+    <div class="{{ $baseClass }}__body">
+      <div class="grid">
+        <div class="grid-xs-3">
+          <a href="{{$logotypeHref}}" class="{{ $baseClass }}__home_link">
+            {{$logotype}}
+          </a>
+        </div>
+        <div class="grid-xs-9">
+          {{$menu}}
+        </div>
       </div>
     </div>
+
   @if($includeContainer)
     </div>
   @endif
