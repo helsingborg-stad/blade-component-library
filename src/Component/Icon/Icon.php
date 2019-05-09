90cf90f75d5740b6a8d624c765b2b1f79cce6dff
Jonatan Hanson <jonatan.hanson@helsingborg.se>
2019-05-09 11:00:36 +0200

    Add base class last

--- src/Component/Icon/Icon.php
+++ src/Component/Icon/Icon.php
@@ -2,20 +2,17 @@
 
 namespace BladeComponentLibrary\Component\Icon;
 
-class Icon extends \BladeComponentLibrary\Component\BaseController  
+class Icon extends \BladeComponentLibrary\Component\BaseController
 {
-    
+
     public function init() {
 
         //Extract array for eazy access (fetch only)
         extract($this->data);
 
-        //Class list
-        $this->data['classList'][] = "c-icon"; 
-
         //Append space before label
         if($label = trim($label)) {
-            $this->data['label'] = " " . $label; 
+            $this->data['label'] = " " . $label;
         }
 
         //Avabile sizes
@@ -26,14 +23,17 @@ class Icon extends \BladeComponentLibrary\Component\BaseController
             'lg' => '48',
             'xl' => '64',
             'xxl' => '80',
-        ]; 
+        ];
+
+        //Add base class
+        array_unshift($this->data['classList'], "c-icon");
 
         //Size class
         if(isset($sizes[$size])) {
-            $this->data['classList'][] = $this->getBaseClass() . "--size-".$sizes[$size]; 
-            $this->data['classList'][] = $this->getBaseClass() . "--size-".$size; 
+            $this->data['classList'][] = $this->getBaseClass() . "--size-".$sizes[$size];
+            $this->data['classList'][] = $this->getBaseClass() . "--size-".$size;
         } else {
-            $this->data['classList'][] = $this->getBaseClass() . "--size-32"; 
+            $this->data['classList'][] = $this->getBaseClass() . "--size-32";
             $this->data['classList'][] = $this->getBaseClass() . "--size-md";
         }
     }
