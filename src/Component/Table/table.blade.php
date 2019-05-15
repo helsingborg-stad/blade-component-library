5e23befffa547a649b4aa34892bb7c83d6c9b9d8
Sebastian Thulin <sebastian.thulin@helsingborg.se>
2019-05-15 12:02:42 +0200

    Table markup

--- src/Component/Table/table.blade.php
+++ src/Component/Table/table.blade.php
@@ -1,41 +1,43 @@
 <!-- table.blade.php -->
 @if($list)
   <div class="{{ $class }}">
-    <table class="{{$baseClass}}__table">
-      @if($showCaption)
-      <caption>{{ $caption }}</caption>
-      @endif
+    <div class="{{$baseClass}}__inner">
+      <table class="{{$baseClass}}__table">
+        @if($showCaption)
+        <caption>{{ $caption }}</caption>
+        @endif
 
-      @if($showHeader)
-      <thead class="{{$baseClass}}__head">
-        <tr>
-          @foreach($headings as $heading)
-            <th scope="col" class="{{$baseClass}}__column {{$baseClass}}__column-{{ $loop->index }}">{{ $heading }}</th>
-          @endforeach
-        </tr>
-      </thead>
-      @endif
-
-      <tbody class="{{$baseClass}}__body">
-        @foreach($list as $row) 
-          <tr class="{{$baseClass}}__line {{$baseClass}}__line-{{ $loop->index }}">
-            @foreach($row as $column) 
-              <td scope="row" class="{{$baseClass}}__column {{$baseClass}}__column-{{ $loop->index }}">{{ $column }}</td>
-            @endforeach
-          </tr>
-        @endforeach
-      </tbody>
-
-      @if($showFooter)
-        <tfoot class="{{$baseClass}}__foot">
-          <tr>
+        @if($showHeader)
+        <thead class="{{$baseClass}}__head">
+          <tr class="{{$baseClass}}__line">
             @foreach($headings as $heading)
               <th scope="col" class="{{$baseClass}}__column {{$baseClass}}__column-{{ $loop->index }}">{{ $heading }}</th>
             @endforeach
           </tr>
-        </tfoot>
-      @endif
-    </table>
+        </thead>
+        @endif
+
+        <tbody class="{{$baseClass}}__body">
+          @foreach($list as $row) 
+            <tr class="{{$baseClass}}__line {{$baseClass}}__line-{{ $loop->index }}">
+              @foreach($row as $column) 
+                <td scope="row" class="{{$baseClass}}__column {{$baseClass}}__column-{{ $loop->index }}">{{ $column }}</td>
+              @endforeach
+            </tr>
+          @endforeach
+        </tbody>
+
+        @if($showFooter)
+          <tfoot class="{{$baseClass}}__foot">
+            <tr class="{{$baseClass}}__line">
+              @foreach($headings as $heading)
+                <th scope="col" class="{{$baseClass}}__column {{$baseClass}}__column-{{ $loop->index }}">{{ $heading }}</th>
+              @endforeach
+            </tr>
+          </tfoot>
+        @endif
+      </table>
+    </div>
   </div>
 @else
   <!-- No table list data -->
