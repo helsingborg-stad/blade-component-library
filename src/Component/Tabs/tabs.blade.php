dcbe201814629c445a9d9b9ef7dc1381c3c80b90
Jonatan Hanson <jonatan.hanson@helsingborg.se>
2019-05-16 09:29:01 +0200

    removed attribute

--- src/Component/Tabs/tabs.blade.php
+++ src/Component/Tabs/tabs.blade.php
@@ -3,7 +3,7 @@
     <{{$componentElement}} class="{{ $class }}" js-expand-container="{{ $baseClass }}" {!! $attribute !!}>
         <div class="{{ $baseClass }}__header">
             @foreach($tabs as $tab)
-                <{{$headingElement}} tabindex="0" role="tab" class="{{$baseClass}}__button" aria-controls="{{ $baseClass }}__aria-{{ $id }}-{{ $loop->index }}" aria-expanded="{{ $loop->index === 0 ? 'true' : 'false' }}" js-disable-toggle="true" js-expand-button="{{ $baseClass }}__button">
+                <{{$headingElement}} tabindex="0" role="tab" class="{{$baseClass}}__button" aria-controls="{{ $baseClass }}__aria-{{ $id }}-{{ $loop->index }}" aria-expanded="{{ $loop->index === 0 ? 'true' : 'false' }}" js-expand-button="{{ $baseClass }}__button">
                     <span class="{{$baseClass}}__button-wrapper" tabindex="-1">
                         {{ $tab['title'] ?? '' }}
                     </span>
