2944c3b3111dd7df5ed25908c8a4a5f7d9242767
Carl Johan Silvergrund <johan.silvergrund@hiq.se>
2019-10-25 17:56:08 +0200

    Removed 50% circle

--- src/Component/Image/Image.php
+++ src/Component/Image/Image.php
@@ -27,41 +27,34 @@ class Image extends \BladeComponentLibrary\Component\BaseController
 			$this->data['alt'] = $this->data['caption'];
 		}
 
-		//Rounded corners
+		//Rounded corners all sides
 		if ($rounded) {
 			$this->data['classList'][] = $this->getBaseClass() . "--rounded ";
 		}
 
-		//Rounded corners top
+		//Rounded corners top left
 		if ($roundedTopLeft) {
 			$this->data['classList'][] = $this->getBaseClass() . "--rounded-top-left ";
 		}
 
-		//Rounded corners top
+		//Rounded corners top right
 		if ($roundedTopRight) {
 			$this->data['classList'][] = $this->getBaseClass() . "--rounded-top-right ";
 		}
 
-		//Rounded corners bottom
+		//Rounded corners bottom left
 		if ($roundedBottomLeft) {
 			$this->data['classList'][] = $this->getBaseClass() . "--rounded-bottom-left ";
 		}
 
-		//Rounded corners bottom
+		//Rounded corners bottom right
 		if ($roundedBottomRight) {
 			$this->data['classList'][] = $this->getBaseClass() . "--rounded-bottom-right ";
 		}
 
-		//Rounded corners bottom
+		//Rounded corners radius
 		if ($roundedRadius) {
 			$this->data['classList'][] = $this->getBaseClass() . "--rounded-".$roundedRadius;
 		}
-
-		//Rounded corners circle
-		if ($roundedCircle) {
-			$this->data['classList'][] = $this->getBaseClass() . "--rounded-circle ";
-			$this->data['classList'][] = $this->getBaseClass() . "--rounded-circle ";
-		}
-
 	}
 }
