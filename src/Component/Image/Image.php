be86814b87920b17e2c1b9bbf83a24ea9daa0d21
Carl Johan Silvergrund <johan.silvergrund@hiq.se>
2019-10-25 15:57:33 +0200

    Radius on images

--- src/Component/Image/Image.php
+++ src/Component/Image/Image.php
@@ -2,31 +2,66 @@
 
 namespace BladeComponentLibrary\Component\Image;
 
-class Image extends \BladeComponentLibrary\Component\BaseController  
+class Image extends \BladeComponentLibrary\Component\BaseController
 {
-    
-    public function init() {
 
-        //Extract array for eazy access (fetch only)
-        extract($this->data);
+	public function init()
+	{
 
-        //Class list
-        $this->data['classList'][] = "c-image"; 
+		//Extract array for eazy access (fetch only)
+		extract($this->data);
+		array_unshift($this->data['classList'], "c-image");
 
-        //Add placeholder class
-        if(!$src) {
-            $this->data['classList'][] = $this->getBaseClass() . "--is-placeholder"; 
-        }
+		//Add placeholder class
+		if (!$src) {
+			$this->data['classList'][] = $this->getBaseClass() . "--is-placeholder";
+		}
 
-        //Make full width
-        if($fullWidth) {
-            $this->data['classList'][] = $this->getBaseClass() . "--full-width"; 
-        }
+		//Make full width
+		if ($fullWidth) {
+			$this->data['classList'][] = $this->getBaseClass() . "--full-width";
+		}
 
-        //Inherit the alt text
-        if(!$alt && $caption) {
-            $this->data['alt'] = $this->data['caption'];
-        }
-        
-    }
+		//Inherit the alt text
+		if (!$alt && $caption) {
+			$this->data['alt'] = $this->data['caption'];
+		}
+
+		//Rounded corners
+		if ($rounded) {
+			$this->data['classList'][] = $this->getBaseClass() . "--rounded ";
+		}
+
+		//Rounded corners top
+		if ($roundedTopLeft) {
+			$this->data['classList'][] = $this->getBaseClass() . "--rounded-top-left ";
+		}
+
+		//Rounded corners top
+		if ($roundedTopRight) {
+			$this->data['classList'][] = $this->getBaseClass() . "--rounded-top-right ";
+		}
+
+		//Rounded corners bottom
+		if ($roundedBottomLeft) {
+			$this->data['classList'][] = $this->getBaseClass() . "--rounded-bottom-left ";
+		}
+
+		//Rounded corners bottom
+		if ($roundedBottomRight) {
+			$this->data['classList'][] = $this->getBaseClass() . "--rounded-bottom-right ";
+		}
+
+		//Rounded corners bottom
+		if ($roundedRadius) {
+			$this->data['classList'][] = $this->getBaseClass() . "--rounded-".$roundedRadius;
+		}
+
+		//Rounded corners circle
+		if ($roundedCircle) {
+			$this->data['classList'][] = $this->getBaseClass() . "--rounded-circle ";
+			$this->data['classList'][] = $this->getBaseClass() . "--rounded-circle ";
+		}
+
+	}
 }
