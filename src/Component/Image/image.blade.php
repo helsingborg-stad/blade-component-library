7f8ae2eb98c16ce07b1a6fcd452a7c026a49df48
Carl Johan Silvergrund <johan.silvergrund@hiq.se>
2019-10-28 13:49:15 +0100

    Changed name on parameter

--- src/Component/Image/image.blade.php
+++ src/Component/Image/image.blade.php
@@ -1,7 +1,7 @@
 <!-- image.blade.php -->
 <figure class="{{ $class }}" {!! $attribute !!}>
     @if($src) 
-        <img src="{{$src}}" alt="{{$alt}}" class="{{$baseClass}}__image" />
+        <img src="{{$src}}" alt="{{$alt}}" class="{{$baseClass}}__image @if($modalId){{$baseClass}}__modal @endif" @if($modalId) data-open="{{$modalId}}" @endif />
         @if($caption)
             <figcaption class="{{$baseClass}}__caption">{{$caption}}</figcaption>
         @endif
