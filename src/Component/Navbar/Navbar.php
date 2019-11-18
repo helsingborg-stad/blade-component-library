066cdf7001ced80d95ee46b323ebfa401ed09cec
Silvergrund <johan.silvergrund@hiq.se>
2019-11-18 11:24:19 +0100

    Squashed commit of the following:
    commit 663f5ec8bf8e6fde6990666be2552346ac271b96
    Author: Alexander Boman Skoug <alexanderbomanskoug@gmail.com>
    Date:   Fri Nov 15 09:12:10 2019 +0100
    
        Navbar
         - Add list handling
         - Add layout handling
    
    commit 33c041b4ce8c282d6ac6ffc1e94fab88240ef00b
    Author: Silvergrund <johan.silvergrund@hiq.se>
    Date:   Thu Nov 14 13:12:35 2019 +0100
    
        Init cards
    
    commit 0d7cf723746762b7b945a48646e0d4a6b468c750
    Author: Silvergrund <johan.silvergrund@hiq.se>
    Date:   Wed Nov 13 11:14:06 2019 +0100
    
        disabled header typography if no content for galleries
    
    commit ffb7596dd2b45ee22fd2904a291ff6ab8a10b662
    Author: Eric Rosenborg <eric.rosenborg@hiq.se>
    Date:   Wed Nov 13 10:08:55 2019 +0100
    
        Dropdown, Toggle, design buttons
    
         Add a new component for dropdown.
         - Select direction in which the popup-menu opens
         - Popup on hover or click
         - Slot contains buttons
    
         Add new design for buttons
    
         Make buttons toggle by adding toggle true
         Make button groups toggle by adding toggle true and by adding js-toggle-trigger
    
    commit d01f751e339321fa323cdeca578b4205138d97d9
    Author: Silvergrund <johan.silvergrund@hiq.se>
    Date:   Wed Nov 13 09:49:42 2019 +0100
    
        Steppers

---
+++ src/Component/Navbar/Navbar.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace BladeComponentLibrary\Component\Navbar;
+
+class Navbar extends \BladeComponentLibrary\Component\BaseController
+{
+    public function init() {
+        //Extract array for eazy access (fetch only)
+        extract($this->data);
+
+        // Add base class
+        array_unshift($this->data['classList'], 'c-navbar');
+
+        if (isset($logoPosition)) {
+            //Placement
+            $placementClass = "c-navbar--logo__";
+            isset($logoPosition) ? $placementClass = $placementClass.$logoPosition : "";
+            
+            if (isset($itemsPosition) && $itemsPosition != $logoPosition) {
+                $placementClass = $placementClass . "__items__" . $itemsPosition;
+            }
+
+            $this->data['classList'][] = $placementClass;
+        }
+
+        $this->data['items'] = array_chunk($items, ceil(count($items)/2), true );
+    }
+}
