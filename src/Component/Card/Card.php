0f26f0a6d6eb453da6c2922165149b7905c364d0
Silvergrund <johan.silvergrund@hiq.se>
2019-11-21 10:01:02 +0100

    Squashed commit of the following:
    commit cd22af6ab5c06ea4e8784be7132070285f93eb4a
    Author: Silvergrund <johan.silvergrund@hiq.se>
    Date:   Thu Nov 21 09:59:53 2019 +0100
    
        Cards - Array key check in controller
    
    commit 3fa7701dfbc86c036505ecf94ce6b44e175a4da3
    Author: Silvergrund <johan.silvergrund@hiq.se>
    Date:   Wed Nov 20 23:03:23 2019 +0100
    
        Cards Controller
    
    commit 9f10378c0b21c0ea181b89ad6a83665b721c35de
    Author: Silvergrund <johan.silvergrund@hiq.se>
    Date:   Wed Nov 20 16:40:54 2019 +0100
    
        card controller
    
    commit 00c756073f384691f8d8a7091a6339fcf739e65d
    Author: Silvergrund <johan.silvergrund@hiq.se>
    Date:   Wed Nov 20 16:40:48 2019 +0100
    
        Card controller
    
    commit 68b93af1140c22b430f5c9c82ced1b254e6ae4df
    Author: Silvergrund <johan.silvergrund@hiq.se>
    Date:   Wed Nov 20 16:36:12 2019 +0100
    
        Card controller
    
    commit cb14b244c5a8bb0bd22bf7083385c649c868b5c6
    Author: Silvergrund <johan.silvergrund@hiq.se>
    Date:   Wed Nov 20 15:15:44 2019 +0100
    
        var typo

--- src/Component/Card/Card.php
+++ src/Component/Card/Card.php
@@ -4,26 +4,57 @@ namespace BladeComponentLibrary\Component\Card;
 
 class Card extends \BladeComponentLibrary\Component\BaseController
 {
-    public function init() {
-        //Extract array for eazy access (fetch only)
-        extract($this->data);
+	public function init()
+	{
+		//Extract array for eazy access (fetch only)
+		extract($this->data);
 
-        //Mark as link
-        if($href) {
-            $this->data['classList'][] = $this->getBaseClass() . "--link";
-        }
+		//Mark as link
+		if ($href) {
+			$this->data['classList'][] = $this->getBaseClass() . "--link";
+		}
 
-        //Show body and Footer depending on data
+		//Card Body & Card Footer
 		$this->data['showBody'] = (!empty(array_filter([$title, $content, $byline]))) ? true : false;
 		$this->data['showFooter'] = (!empty(array_filter([$buttons])) || !empty(array_filter([$icons]))) ? true : false;
 
-        //Has ripple
-        if($hasRipple) {
-            $this->data['classList'][] = "ripple"; 
-            $this->data['classList'][] = "ripple--before"; 
-        }
+		// Avatar
+		$this->data['avatarImage'] = (isset($avatar["image"]) && !empty($avatar['image'])) ? $avatar['image'] : null;
+		$this->data['avatarName'] = (isset($avatar["name"]) && !empty($avatar['name'])) ? $avatar['name'] : null;
 
-        // Add base class
-        array_unshift($this->data['classList'], 'c-card');
-    }
+		// Title and Byline
+		$this->data['byline']["position"] = (isset($byline["position"]) && !empty($byline['position'])) ? $byline['position'] : null;
+		$this->data['byline']["text"] = (isset($byline["text"]) && !empty($byline['position'])) ? $byline['text'] : null;
+		$this->data['title']["position"] = (isset($title["position"]) && !empty($title['position'])) ? $title['position'] : null;
+		$this->data['title']["text"] = (isset($title["text"]) && !empty($title['position'])) ? $title['text'] : null;
+
+		// Check if Button icon parameter array has keys
+		$this->data['icons'] = (!empty(array_filter($icons))) ? $icons : false;
+		if ($this->data['icons']) {
+			foreach($this->data['icons'] as $key => $iconParams){
+				$this->data['icons'][$key]['color']  = array_key_exists('color', $iconParams) ? $this->data['icons'][$key]['color']  : '';
+				$this->data['icons'][$key]['size']  = array_key_exists('size', $iconParams) ? $this->data['icons'][$key]['size']  : '';
+				$this->data['icons'][$key]['attributeList']  = array_key_exists('name', $iconParams) ? $this->data['icons'][$key]['attributeList']  : '';
+			}
+		}
+
+		// Check if Button parameter array has keys
+		$this->data['buttons'] = (!empty(array_filter($buttons))) ? $buttons : false;
+		if ($this->data['buttons']) {
+			foreach($this->data['buttons'] as $key => $buttonParams){
+				$this->data['buttons'][$key]['href']  = array_key_exists('href', $buttonParams) ? $this->data['buttons'][$key]['href']  : '';
+				$this->data['buttons'][$key]['text']  = array_key_exists('text', $buttonParams) ? $this->data['buttons'][$key]['text']  : '';
+				$this->data['buttons'][$key]['name']  = array_key_exists('name', $buttonParams) ? $this->data['buttons'][$key]['name']  : '';
+			}
+		}
+
+		//Has ripple
+		if ($hasRipple) {
+			$this->data['classList'][] = "ripple";
+			$this->data['classList'][] = "ripple--before";
+		}
+
+		// Add base class
+		array_unshift($this->data['classList'], 'c-card');
+	}
 }
