cd22af6ab5c06ea4e8784be7132070285f93eb4a
Silvergrund <johan.silvergrund@hiq.se>
2019-11-21 09:59:53 +0100

    Cards - Array key check in controller

--- src/Component/Card/Card.php
+++ src/Component/Card/Card.php
@@ -14,16 +14,39 @@ class Card extends \BladeComponentLibrary\Component\BaseController
 			$this->data['classList'][] = $this->getBaseClass() . "--link";
 		}
 
-		//Data
+		//Card Body & Card Footer
 		$this->data['showBody'] = (!empty(array_filter([$title, $content, $byline]))) ? true : false;
 		$this->data['showFooter'] = (!empty(array_filter([$buttons])) || !empty(array_filter([$icons]))) ? true : false;
 
+		// Avatar
 		$this->data['avatarImage'] = (isset($avatar["image"]) && !empty($avatar['image'])) ? $avatar['image'] : null;
 		$this->data['avatarName'] = (isset($avatar["name"]) && !empty($avatar['name'])) ? $avatar['name'] : null;
-		
-		$this->data['icons'] = (!empty(array_filter($icons))) ? $icons : null;
-		#$this->data['icons'] = (!empty(array_filter($icons['color']))) ? $icons['color'] : null;
 
+		// Title and Byline
+		$this->data['byline']["position"] = (isset($byline["position"]) && !empty($byline['position'])) ? $byline['position'] : null;
+		$this->data['byline']["text"] = (isset($byline["text"]) && !empty($byline['position'])) ? $byline['text'] : null;
+		$this->data['title']["position"] = (isset($title["position"]) && !empty($title['position'])) ? $title['position'] : null;
+		$this->data['title']["text"] = (isset($title["text"]) && !empty($title['position'])) ? $title['text'] : null;
+
+		// Check if Button icon parameter array has keys
+		$this->data['icons'] = (!empty(array_filter($icons))) ? $icons : false;
+		if ($this->data['icons']) {
+			foreach($this->data['icons'] as $key => $iconParams){
+				$this->data['icons'][$key]['color']  = array_key_exists('color', $iconParams) ? $this->data['icons'][$key]['color']  : '';
+				$this->data['icons'][$key]['size']  = array_key_exists('size', $iconParams) ? $this->data['icons'][$key]['size']  : '';
+				$this->data['icons'][$key]['attributeList']  = array_key_exists('name', $iconParams) ? $this->data['icons'][$key]['attributeList']  : '';
+			}
+		}
+
+		// Check if Button parameter array has keys
+		$this->data['buttons'] = (!empty(array_filter($buttons))) ? $buttons : false;
+		if ($this->data['buttons']) {
+			foreach($this->data['buttons'] as $key => $buttonParams){
+				$this->data['buttons'][$key]['href']  = array_key_exists('href', $buttonParams) ? $this->data['buttons'][$key]['href']  : '';
+				$this->data['buttons'][$key]['text']  = array_key_exists('text', $buttonParams) ? $this->data['buttons'][$key]['text']  : '';
+				$this->data['buttons'][$key]['name']  = array_key_exists('name', $buttonParams) ? $this->data['buttons'][$key]['name']  : '';
+			}
+		}
 
 		//Has ripple
 		if ($hasRipple) {
