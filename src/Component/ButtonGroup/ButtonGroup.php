cf3cf1c9cae9f47c6b57f8081b7ea85500b7fa21
Silvergrund <johan.silvergrund@hiq.se>
2019-11-25 14:24:58 +0100

    Squashed commit of the following:
    commit 76779a5a08dd7ec2b7dc8a0cfc5c0751a80292b7
    Author: Silvergrund <johan.silvergrund@hiq.se>
    Date:   Mon Nov 25 14:22:54 2019 +0100
    
        button and buttongroup fix

--- src/Component/ButtonGroup/ButtonGroup.php
+++ src/Component/ButtonGroup/ButtonGroup.php
@@ -10,7 +10,7 @@ class ButtonGroup extends \BladeComponentLibrary\Component\BaseController
         extract($this->data);
 
         // Base class
-        array_unshift($this->data['classList'], 'c-btn-group');
+        array_unshift($this->data['classList'], 'c-buttongroup');
 
         if(isset($toggle) && $toggle){
             $this->data['container'] = 'js-toggle-container';
@@ -21,7 +21,7 @@ class ButtonGroup extends \BladeComponentLibrary\Component\BaseController
         }
 
         if(isset($backgroundColor)){
-            $this->data['classList'][] = $this->getBaseClass() . ' .c-btn--' . $backgroundColor; 
+            $this->data['classList'][] = $this->getBaseClass() . ' .c-button--' . $backgroundColor;
         }
         
     }
