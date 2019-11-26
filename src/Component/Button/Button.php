4900578752069190a823a0f4b3e7f42af2fb0b18
Silvergrund <johan.silvergrund@hiq.se>
2019-11-26 14:14:35 +0100

    Merge

--- src/Component/Button/Button.php
+++ src/Component/Button/Button.php
@@ -5,68 +5,65 @@ namespace BladeComponentLibrary\Component\Button;
 class Button extends \BladeComponentLibrary\Component\BaseController
 {
 
-    public function init() {
-        //Extract array for eazy access (fetch only)
-        extract($this->data);
+	public function init()
+	{
+		//Extract array for eazy access (fetch only)
+		extract($this->data);
 
-        $this->data['labelMod'] = "";
-       
-        $this->data['id'] = uniqid("", true);
-        
-        if (isset($isIconButton) && $isIconButton) {
-            $this->data['classList'][] = $this->getBaseClass() . "__icon"; 
-        } elseif (isset($isOutlined) && $isOutlined) {
-            $this->data['classList'][] = $this->getBaseClass() . "__outlined--" . $color; 
-            if(isset($toggle) && $toggle){
-                $this->data['attributeList']['js-toggle-self'] = '';
-            }
-        } else{
-            $this->data['classList'][] = $this->getBaseClass() . '__text';
-            $this->data['classList'][] = $this->getBaseClass() . '__text--' . $color;
-        }
+		$this->data['labelMod'] = "";
 
-        if (isset($background) && $background && !$isOutlined) {
-            $this->data['classList'][] = $this->getBaseClass() . "--" . $background; 
-        }
+		$this->data['id'] = uniqid("", true);
 
-        if (isset($icon) && isset($reverseIcon) && $reverseIcon){
-                $this->data['labelMod'] = '--reverse';    
-        }
-    
-        if (!$href) {
-            $this->data['href'] = "#";
-        }
+		if (isset($isIconButton) && $isIconButton) {
+			$this->data['classList'][] = $this->getBaseClass() . "__icon";
+		} elseif (isset($isOutlined) && $isOutlined) {
+			$this->data['classList'][] = $this->getBaseClass() . "__outlined--" . $color;
+			if (isset($toggle) && $toggle) {
+				$this->data['attributeList']['js-toggle-self'] = '';
+			}
+		} else {
+			$this->data['classList'][] = $this->getBaseClass() . '__text';
+			$this->data['classList'][] = $this->getBaseClass() . '__text--' . $color;
+		}
 
-        if (isset($floating) && $floating['animate'] && $floating['hover']) {
-            $this->data['classList'][] = $this->getBaseClass() . "--animated-float-on-hover";
-        }elseif (isset($floating) && $floating['hover']) {
-            $this->data['classList'][] = $this->getBaseClass() . "--float-on-hover";
-        }elseif (isset($floating) && is_array($floating)){
-            $this->data['classList'][] = $this->getBaseClass() . "--floating";
-        }
+		if (isset($background) && $background && !$isOutlined) {
+			$this->data['classList'][] = $this->getBaseClass() . "--" . $background;
+		}
 
-        if (isset($size) && !isset($isIconButton)) {
-            $this->data['classList'][] = $this->getBaseClass() . "--" . $size;
-        }
+		if (isset($icon) && isset($reverseIcon) && $reverseIcon) {
+			$this->data['labelMod'] = '--reverse';
+		}
 
-        if (isset($hasRipple) && $hasRipple) {
-            $this->data['classList'][] = "ripple"; 
-            $this->data['classList'][] = "ripple--before"; 
-        }
-        
-        if (isset($icon)) {
-            $this->data['icon'] = $icon; 
-        }
+		if (!$href) {
+			$this->data['href'] = "#";
+		}
 
-        if (isset($floatOnHover) && $floatOnHover) {
-            $this->data['classList'][] = $this->getBaseClass() . "--float-on-hover";
-        }
-<<<<<<< HEAD
+		if (isset($floating) && $floating['animate'] && $floating['hover']) {
+			$this->data['classList'][] = $this->getBaseClass() . "--animated-float-on-hover";
+		} elseif (isset($floating) && $floating['hover']) {
+			$this->data['classList'][] = $this->getBaseClass() . "--float-on-hover";
+		} elseif (isset($floating) && is_array($floating)) {
+			$this->data['classList'][] = $this->getBaseClass() . "--floating";
+		}
+
+		if (isset($size) && !isset($isIconButton)) {
+			$this->data['classList'][] = $this->getBaseClass() . "--" . $size;
+		}
+
+		if (isset($hasRipple) && $hasRipple) {
+			$this->data['classList'][] = "ripple";
+			$this->data['classList'][] = "ripple--before";
+		}
+
+		if (isset($icon)) {
+			$this->data['icon'] = $icon;
+		}
+
+		if (isset($floatOnHover) && $floatOnHover) {
+			$this->data['classList'][] = $this->getBaseClass() . "--float-on-hover";
+		}
+
 		$this->data['attributeList']['tabindex'] = "1";
 	}
-=======
-
-        $this->data['attributeList']['tabindex'] = "1"; 
-    }
->>>>>>> 6500621423095f79953e698cdb4b1f410fe77f79
 }
+
