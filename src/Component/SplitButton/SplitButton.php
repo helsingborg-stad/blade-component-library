ea9e6353589a9a5ab2d97f75e7243f7ea020adc0
Sebastian Thulin <sebastian.thulin@helsingborg.se>
2019-11-26 09:58:35 +0100

    Upeprcase/lowercase fix

--- src/Component/SplitButton/SplitButton.php
+++ src/Component/SplitButton/SplitButton.php
@@ -1,16 +1,12 @@
 <?php
 
-namespace BladeComponentLibrary\Component\SplitButton;
+namespace BladeComponentLibrary\Component\Splitbutton;
 
-class SplitButton extends \BladeComponentLibrary\Component\BaseController
+class Splitbutton extends \BladeComponentLibrary\Component\BaseController
 {
 
     public function init() {
         //Extract array for eazy access (fetch only)
         extract($this->data);
-
-        // Base class
-        array_unshift($this->data['classList'], 'c-splitbutton');
-
     }
 }
--- src/Component/Splitbutton/splitbutton.blade.php
+++ src/Component/Splitbutton/splitbutton.blade.php
@@ -3,18 +3,18 @@
     @button(['background' => $backgroundColor, 'text' => $buttonText])
     @endbutton
     @dropdown([
-        'items' => $items,
-        'direction' => $dropdownDirection,
-        'popup' => 'click',
-        'itemElement' => 'div'
+    'items' => $items,
+    'direction' => $dropdownDirection,
+    'popup' => 'click',
+    'itemElement' => 'div'
+    ])
+        @button([
+            'isIconButton' => true,
+            'icon' => $icon,
+            'size' => 'md',
+            'background' => $backgroundColor
+            
         ])
-            @button([
-                'isIconButton' => true,
-                'icon' => $icon,
-                'size' => 'md',
-                'background' => $backgroundColor
-
-            ])
         @endbutton
     @enddropdown
 @endbuttonGroup
