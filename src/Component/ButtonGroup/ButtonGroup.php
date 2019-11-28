79dd24ef43968328116df44fd4e2fb3f1b01b764
Silvergrund <johan.silvergrund@hiq.se>
2019-11-28 17:07:26 +0100

    fix

--- src/Component/ButtonGroup/ButtonGroup.php
+++ src/Component/ButtonGroup/ButtonGroup.php
@@ -2,34 +2,27 @@
 
 namespace BladeComponentLibrary\Component\ButtonGroup;
 
+/**
+ * Class ButtonGroup
+ * @package BladeComponentLibrary\Component\ButtonGroup
+ */
 class ButtonGroup extends \BladeComponentLibrary\Component\BaseController
 {
-    public function init() {
-      
-        //Extract array for eazy access (fetch only)
-        extract($this->data);
+	public function init() {
 
-<<<<<<< HEAD
-        // Base class
-        array_unshift($this->data['classList'], 'c-buttongroup');
+		//Extract array for eazy access (fetch only)
+		extract($this->data);
 
-=======
->>>>>>> origin/develop
-        if(isset($toggle) && $toggle){
-            $this->data['container'] = 'js-toggle-container';
-        }
+		if(isset($toggle) && $toggle){
+			$this->data['container'] = 'js-toggle-container';
+		}
 
-        if(isset($borderColor)){
-            $this->data['classList'][] = $this->getBaseClass() . '__border--' . $borderColor; 
-        }
+		if(isset($borderColor)){
+			$this->data['classList'][] = $this->getBaseClass() . '__border--' . $borderColor;
+		}
 
-        if(isset($backgroundColor)){
-<<<<<<< HEAD
-            $this->data['classList'][] = $this->getBaseClass() . ' .c-button--' . $backgroundColor;
-=======
-            $this->data['classList'][] = $this->getBaseClass() . '--' . $backgroundColor; 
->>>>>>> origin/develop
-        }
-       
-    }
+		if(isset($backgroundColor)){
+			$this->data['classList'][] = $this->getBaseClass() . '--' . $backgroundColor;
+		}
+	}
 }
