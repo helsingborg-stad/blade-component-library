ec14a4b6df58f7058b85dcb454134fae1d082157
Alexander Boman Skoug <alexanderbomanskoug@gmail.com>
2019-11-29 17:38:48 +0100

    Navbar - Clean Uo  - Add default values  - Improved php  - Add handler for colours

--- src/Component/Navbar/Navbar.php
+++ src/Component/Navbar/Navbar.php
@@ -2,24 +2,84 @@
 
 namespace BladeComponentLibrary\Component\Navbar;
 
+/**
+ * Class Card
+ * @package BladeComponentLibrary\Component\Navbar
+ */
 class Navbar extends \BladeComponentLibrary\Component\BaseController
 {
     public function init() {
-        //Extract array for eazy access (fetch only)
-        extract($this->data);
 
-        if (isset($logoPosition)) {
+        // Extract array for eazy access (fetch only)
+        extract($this->data);
+        
+        // Encapsulates component data expose it globally for class
+        $this->compParams = [
+            'logoPosition'  => $logoPosition,
+            'linksPosition' => $linksPosition,
+            'topAccent'     => $topAccent,
+            'activeAccent'  => $activeAccent
+        ];
+
+        // Set the position of the logo
+        $this->setLogoPosition();
+
+        // Set the colour of the top accent
+        $this->setTopAccent();
+
+        // Set the colour of the active & hover accent
+        $this->setActiveAccent();
+
+        // Splits the nav items in half for layout
+        $this->data['items'] = array_chunk($items, ceil(count($items)/2), true );
+    }
+
+    /**
+	 * Builds the class for position of the logo
+	 * @return array
+	 */
+    public function setLogoPosition() {
+        if (isset($this->compParams['logoPosition'])) {
             //Placement
             $placementClass = "c-navbar--logo__";
-            isset($logoPosition) ? $placementClass = $placementClass.$logoPosition : "";
+
+            isset($this->compParams['logoPosition']) ? $placementClass
+                = $placementClass.$this->compParams['logoPosition'] : "";
             
-            if (isset($itemsPosition) && $itemsPosition != $logoPosition) {
-                $placementClass = $placementClass . "__items__" . $itemsPosition;
+            if (!empty($this->compParams['linksPosition']) &&
+                $this->compParams['linksPosition'] != $this->compParams['logoPosition']) {
+                    $placementClass = $placementClass . "__items__" . $this->compParams['linksPosition'];
             }
 
             $this->data['classList'][] = $placementClass;
         }
 
-        $this->data['items'] = array_chunk($items, ceil(count($items)/2), true );
+        return $this->data;
+    }
+
+    /**
+	 * Builds class for top accent
+	 * @return array
+	 */
+    public function setTopAccent() {
+        if (isset($this->compParams['topAccent'])) {
+            $this->data['classList'][] =
+                "c-navbar__top__accent--".$this->compParams['topAccent'];
+        }
+
+        return $this->data;
+    }
+
+    /**
+	 * Builds class for active & hover state colour
+	 * @return array
+	 */
+    public function setActiveAccent() {
+        if (isset($this->compParams['topAccent'])) {
+            $this->data['classList'][] =
+                "c-navbar__active__accent--" . $this->compParams['activeAccent'];
+        }
+
+        return $this->data;
     }
 }
