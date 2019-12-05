42929d17bc549fc182fefeae193a760ef233db21
Silvergrund <johan.silvergrund@hiq.se>
2019-12-05 17:09:21 +0100

    Fields

--- src/Component/Field/Field.php
+++ src/Component/Field/Field.php
@@ -8,5 +8,16 @@ class Field extends \BladeComponentLibrary\Component\Form\Form
     {
         //Extract array for eazy access (fetch only)
         extract($this->data);
+
+        $this->compParams = [
+            'textarea' => $textarea ?? false,
+            'label' => $label ?? '',
+            'type' => $type ?? 'text'
+        ];
+
+        $this->data['textarea'] = $this->compParams['textarea'];
+        $this->data['label'] = $this->compParams['label'];
+        $this->data['type'] = $this->compParams['type'];
     }
+
 }
--- src/Component/Field/field.blade.php
+++ src/Component/Field/field.blade.php
@@ -1,9 +1,19 @@
 <!-- field.blade.php -->
-<div id="{{ $id }}" class="{{$class}}">
+<div id="{{ $id }}" class="{{$class}} c-field__{{$type}}">
     @if(!$textarea)
-        <input {!! $attribute !!} >
+        @if ($type === 'radio' || $type === 'checkbox')
+            <input {!! $attribute !!} type="{{$type}}" class="c-field__{{$type}}--hidden-box" id="trigger_{{ $id }}" />
+            <label for="trigger_{{ $id }}" class="c-field__{{$type}}--label">
+                <span class="c-field__{{$type}}--label-box"></span>
+                 <span class="c-field__{{$type}}--label-text">{{$label}}</span>
+            </label>
+        @else
+            <input {!! $attribute !!} />
+            <label class="c-field__text--label">{{$label}}</label>
+        @endif
     @else
         <textarea {!! $attribute !!} ></textarea>
     @endif
-    <label>{{$label}}</label>
+
+
 </div>
