814d135e7b60dc3e12ba2c51845a09dfea7e697b
Silvergrund <johan.silvergrund@hiq.se>
2019-12-10 15:27:34 +0100

    Field validation

--- src/Component/Field/field.blade.php
+++ src/Component/Field/field.blade.php
@@ -3,33 +3,32 @@
     @if(!$textarea)
         @if ($type === 'radio' || $type === 'checkbox')
 
-            <input class="c-field__input" {!! $attribute !!} type="{{$type}}"
+            <input {!! $attribute !!} type="{{$type}}"
                    class="c-field__{{$type}}--hidden-box"
-                   id="trigger_{{ $id }}" />
+                   id="trigger_{{ $id }}"/>
             <label for="trigger_{{ $id }}" class="c-field__{{$type}}--label">
                 <span class="c-field__{{$type}}--label-box"></span>
-                 <span class="c-field__{{$type}}--label-text">{{$label}}</span>
+                <span class="c-field__{{$type}}--label-text">{{$label}}</span>
             </label>
-
         @else
-            <input id="input_{{ $id }}" class="c-field__input"
+            <input id="input_{{ $id }}"
                    value="{{$value}}"
                    {!! $attribute !!}
                    @if($required)
                        required
                        data-required="1"
-                   @endif
+                    @endif
             />
             <label class="c-field__text--label">{{$label}}</label>
-            <div id="error_input_{{ $id }}_message" class="c-field__input-invalid-message">
-                @icon([
-                    'icon' => 'error',
-                    'size' => 'sm'
-                ])
-                @endicon
-                <span class="errorText"></span>
-            </div>
         @endif
+        <div id="error_input_{{ $id }}_message" class="c-field__input-invalid-message">
+            @icon([
+                'icon' => 'error',
+                'size' => 'sm'
+            ])
+            @endicon
+            <span class="errorText"></span>
+        </div>
     @else
         <textarea {!! $attribute !!} ></textarea>
         <label class="c-field__textare--label">{{$label}}</label>
