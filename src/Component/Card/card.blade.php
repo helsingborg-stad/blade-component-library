a60673f7f10fa3be82a3cc5f35f3d9d68d51a8d8
Silvergrund <johan.silvergrund@hiq.se>
2019-12-19 10:26:35 +0100

    Card doc refactor

--- src/Component/Card/card.blade.php
+++ src/Component/Card/card.blade.php
@@ -17,17 +17,16 @@
             @include('Card.sub.card-image')
         @endif
 
-        @if($slot != "")
-            <div class="{{$baseClass}}__middle">{{ $slot }}</div>
-        @endif
-
         @if($showBody)
             <div class="{{$baseClass}}__body">
-                @include('Card.sub.card-body')
-                @if($accordion)
-                    @include('Card.sub.card-accordion')
+                @if($slot !== "")
+                {{ $slot }}
                 @endif
+                <!-- BODY -->
+                @include('Card.sub.card-body')
+                <!-- /BODY -->
             </div>
+
         @endif
 
         @if($showFooter)
@@ -40,6 +39,7 @@
 
                 @if($icons)
                     @include('Card.sub.card-icons')
+
                 @endif
 
                 @if($dropdown && $dropdown['position'] === 'bottom')
@@ -56,4 +56,3 @@
     @endpaper
 
 </div>
-
