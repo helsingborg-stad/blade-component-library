c4ce4db4410e9289a90b4f9556478d5bae23d45d
Silvergrund <johan.silvergrund@hiq.se>
2020-01-07 14:36:04 +0100

    form

--- src/Component/Form/form.blade.php
+++ src/Component/Form/form.blade.php
@@ -1,6 +1,6 @@
 <!-- form.blade.php -->
 <div class="{{$baseClass}}">
-    <form id="{{ $id }}" class="{{$class}}" method="{{$method}}" action="{{$action}}"  {!! $attribute !!}>
+    <form id="{{ $id }}" class="{{$class}}" {!! $attribute !!} method="{{$method}}" action="{{$action}}">
         {!! $slot !!}
     </form>
 </div>
