c188a43a40ab2bfeee4b6a98ccb373e70087e636
Silvergrund <johan.silvergrund@hiq.se>
2020-01-09 13:47:04 +0100

    Option

--- src/Component/Field/field.blade.php
+++ src/Component/Field/field.blade.php
@@ -1,26 +1,15 @@
 <!-- field.blade.php -->
 <div id="{{ $id }}" class="{{$class}} c-field__{{$type}}">
-    @if ($type === 'radio' || $type === 'checkbox')
-        <input {!! $attribute !!} type="{{$type}}"
-               class="c-field__{{$type}}--hidden-box"
-               id="trigger_{{ $id }}"
-               placeholder="{{$label}}"/>
-        <label for="trigger_{{ $id }}" class="c-field__{{$type}}--label">
-            <span class="c-field__{{$type}}--label-box"></span>
-            <span class="c-field__{{$type}}--label-text">{{$label}}</span>
-        </label>
-    @else
-        <input id="input_{{ $id }}"
-               value="{{$value}}"
-               {!! $attribute !!}
-               @if($required)
-                   required
-                   data-required="1"
-                @endif
-                placeholder="{{$label}}"
-        />
-        <label class="c-field__text--label">{{$label}}</label>
-    @endif
+    <input id="input_{{ $id }}"
+           value="{{$value}}"
+           {!! $attribute !!}
+           @if($required)
+               required
+               data-required="1"
+           @endif
+           placeholder="{{$label}}"
+    />
+    <label class="c-field__text--label">{{$label}}</label>
     <div id="error_input_{{ $id }}_message" class="c-field__input-invalid-message">
         @icon([
             'icon' => 'error',
