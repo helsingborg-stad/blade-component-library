841255e7d1b03e4201d8ef4fa09d43bd73a0d7d6
Alexander Boman Skoug <alexanderbomanskoug@gmail.com>
2020-01-10 12:39:33 +0100

    Suggested changes done

--- src/Component/Date/Date.php
+++ src/Component/Date/Date.php
@@ -14,9 +14,9 @@ class Date extends \BladeComponentLibrary\Component\BaseController
         extract($this->data);
 
         if ($action == "timesince") {
-            $this->data['refinedDate'] = $this->getTimeSince(strtotime($timestamp));
+            $this->data['refinedDate'] = $this->convertToHumanReadableUnit(strtotime($timestamp), true);
         } else if ($action == "timeuntil") {
-            $this->data['refinedDate'] = $this->getTimeUntil(strtotime($timestamp));
+            $this->data['refinedDate'] = $this->convertToHumanReadableUnit(strtotime($timestamp));
         }
     }
 
@@ -25,8 +25,8 @@ class Date extends \BladeComponentLibrary\Component\BaseController
      * @param Date A timestamp of which date to count since
      * @return String Time since in words
 	 */
-    public function getTimeSince($time) {
-        $time = time() - $time; // to get the time since that moment
+    private function convertToHumanReadableUnit($time, $timeSince = false) {
+        $time = $timeSince ? time() - $time : $time - time();
         $time = ($time < 1) ? 1 : $time;
         $tokens = array (
             31536000 => 'year',
@@ -39,35 +39,9 @@ class Date extends \BladeComponentLibrary\Component\BaseController
         );
 
         foreach ($tokens as $unit => $text) {
-            if ($time < $unit) continue;
             $numberOfUnits = floor($time / $unit);
-            return $numberOfUnits.' '.$text.(($numberOfUnits>1) ? 's' : '');
-        }
 
-    }
-
-    /**
-	 * Get time until a specified date
-     * @param Date A timestamp of which date to count until
-     * @return String Time until in words
-	 */
-    public function getTimeUntil($time) {
-        $time = $time - time() ; // to get the time since that moment
-        $time = ($time < 1) ? 1 : $time;
-        $tokens = array (
-            31536000 => 'year',
-            2592000 => 'month',
-            604800 => 'week',
-            86400 => 'day',
-            3600 => 'hour',
-            60 => 'minute',
-            1 => 'second'
-        );
-
-        foreach ($tokens as $unit => $text) {
-            if ($time < $unit) continue;
-            $numberOfUnits = floor($time / $unit);
-            return $numberOfUnits.' '.$text.(($numberOfUnits>1) ? 's' : '');
+            if ($time > $unit) return $numberOfUnits.' '.$text.(($numberOfUnits>1) ? 's' : '');
         }
 
     }
