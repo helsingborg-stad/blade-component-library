fb80924da3d72090d32b0fdff4f86591e7de7404
Alexander Boman Skoug <alexanderbomanskoug@gmail.com>
2020-01-15 12:50:41 +0100

    Tags  - add controller for empty fields

--- src/Component/Tags/Tags.php
+++ src/Component/Tags/Tags.php
@@ -7,5 +7,21 @@ class Tags extends \BladeComponentLibrary\Component\BaseController
     public function init() {
         //Extract array for eazy access (fetch only)
         extract($this->data);
+
+        $this->data['tags'] = $this->arrayCleanUp($tags);
+    }
+
+    private function arrayCleanUp($arr) {
+        $filteredTags = [];
+
+        foreach ($arr as $tag) {
+            if (!array_key_exists ( 'href' , $tag )) $tag['href'] = "#";
+            if (!array_key_exists ( 'label' , $tag )) $tag['label'] = "No label";
+            if (!array_key_exists ( 'color' , $tag )) $tag['color'] = "default";
+
+            $filteredTags[] = $tag;
+        }
+
+        return $filteredTags;
     }
 }
