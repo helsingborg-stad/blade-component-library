a0703bc01c94424e649df12e77e143919315793d
Alexander Boman Skoug <alexanderbomanskoug@gmail.com>
2020-01-20 15:05:45 +0100

    Added cta align

--- src/Component/Segment/sub/segment-full.blade.php
+++ src/Component/Segment/sub/segment-full.blade.php
@@ -1,28 +1,31 @@
 <!-- segment.blade.php > /sub/segment-full.blade.php -->
 <div class="{{ $baseClass }}__content">
-  <div class="{{ $baseClass }}__article">
-    
-    @if($article_heading)
-      <div class="{{ $baseClass }}__heading">
-        @typography($article_heading)
-            {{ $article_heading['slot'] }}
-        @endtypography
-      </div>  
-    @endif
+    <div class="{{ $baseClass }}__article">
+        
+        @if($heading)
+            @typography(["variant" => "h1",
+                "element" => "h2",
+                'classList' => [$baseClass.'__heading']
+            ])
+                {{ $heading }}
+            @endtypography
+        @endif
 
-    @if($article_body)
-      <div class="{{ $baseClass }}__body">
-        {{ $article_body }}
-      </div>
-    @endif
+        @if($body)
+            @typography([
+                'variant' => 'body',
+                'element' => 'p',
+                'classList' =>  [$baseClass.'__body']
+            ])
+                {{ $body }}
+            @endtypography
+        @endif
 
-    <div class="{{ $baseClass }}__cta">
-      @foreach($cta as $button)      
-        @button($button)
-        @endbutton
-      @endforeach
+        <div class="{{ $baseClass }}__cta">
+            @foreach($cta as $button)      
+                @button($button)
+                @endbutton
+            @endforeach
+        </div>
     </div>
-
-
-  </div>
 </div>
