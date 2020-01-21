66a9f09cc162e446f6880a2d207db4c26f06b632
Alexander Boman Skoug <alexanderbomanskoug@gmail.com>
2020-01-21 16:39:20 +0100

    Tiles  - Add tiles

---
+++ src/Component/Tile/Tile.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace BladeComponentLibrary\Component\Tile;
+
+class Tile extends \BladeComponentLibrary\Component\BaseController
+{
+    public function init()
+    {
+		//Extract array for eazy access (fetch only)
+        extract($this->data);
+
+        $this->setSize($width, $height);
+        $this->setBackgroundImage($backgroundImage);
+    }
+
+    private static function getUnique()
+    {
+		return uniqid();
+    }
+    
+    private function setSize($width, $height)
+    {
+        $this->data['classList'][] = $this->getBaseClass() . '__item--width' . $width;
+        $this->data['classList'][] = $this->getBaseClass() . '__item--height' . $height;
+    }
+
+    private function setBackgroundImage($img) {
+        if ($img != "")
+            $this->data['attributeList']['style'] = 'background-image: url(' . $img . ')';
+    }
+}
