e267ce2115de6f7e8cc464554ec32ad42f752b20
Alexander Boman Skoug <alexanderbomanskoug@gmail.com>
2020-01-29 10:54:34 +0100

    Hot fix: Removed unnecessary element

--- src/Component/Typography/typography.blade.php
+++ src/Component/Typography/typography.blade.php
@@ -1,6 +1,4 @@
 <!-- typography.blade.php -->
 <{{ $element }} id="{{ $id }}" class="{{ $class }}" {!! $attribute !!}>
-  <span class="{{ $baseClass }}__inner">
     {{ $slot }}
-  </span>
 </{{ $element }}>
