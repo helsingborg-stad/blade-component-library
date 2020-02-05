83f15676065d9075bc11050d921b4f6dc74e68f6
Alexander Boman Skoug <alexanderbomanskoug@gmail.com>
2020-02-05 14:57:32 +0100

    Sidebar and Navbar  - Add Sidebar  - Add Sidebar Example  - Add new master layout  - Rm various navbar layouts

--- src/Component/Navbar/Navbar.php
+++ src/Component/Navbar/Navbar.php
@@ -12,57 +12,5 @@ class Navbar extends \BladeComponentLibrary\Component\BaseController
 
         // Extract array for eazy access (fetch only)
         extract($this->data);
-
-        // Set the position of the logo
-        $this->setLogoPosition($logoPosition, $linksPosition);
-
-        // Set the colour of the top accent
-        $this->setTopAccent($topAccent);
-
-        // Set the colour of the active & hover accent
-        $this->setActiveAccent($activeAccent);
-
-        // Splits the nav items in half for layout
-        $this->data['items'] = array_chunk($items, ceil(count($items)/2), true );
-    }
-
-    /**
-	 * Builds the class for position of the logo
-	 */
-    public function setLogoPosition($logoPosition, $linksPosition) {
-        if ($logoPosition) {
-            //Placement
-            $placementClass = "c-navbar--logo__";
-
-            $logoPosition ? $placementClass
-                = $placementClass.$logoPosition : "";
-            
-            if (!empty($linksPosition) &&
-                $linksPosition != $logoPosition) {
-                    $placementClass = $placementClass . "__items__" . $linksPosition;
-            }
-
-            $this->data['classList'][] = $placementClass;
-        }
-    }
-
-    /**
-	 * Builds class for top accent
-	 */
-    public function setTopAccent($topAccent) {
-        if ($topAccent) {
-            $this->data['classList'][] =
-                "c-navbar__top__accent--".$topAccent;
-        }
-    }
-
-    /**
-	 * Builds class for active & hover state colour
-	 */
-    public function setActiveAccent($activeAccent) {
-        if ($activeAccent) {
-            $this->data['classList'][] =
-                "c-navbar__active__accent--" . $activeAccent;
-        }
     }
 }
---
+++ src/Component/Sidebar/sidebar.blade.php
@@ -0,0 +1,29 @@
+<!-- sidebar.blade.php -->
+<nav id="{{ $id }}" class="{{$class}} c-sidebar--collapsed" {!! $attribute !!} js-sidebar>
+
+    @if (!empty($logo))
+        <div class="{{$baseClass}}__logo">
+            @image([
+                'src'=> $logo,
+                'alt' => "A logo"
+                ])
+            @endimage
+        </div>
+    @endif
+
+    @if ($showHideButton)
+        <div class="u-display--none@md u-display--none@lg u-display--none@xl">
+            <a class="{{$baseClass}}__hide">
+                @icon(['icon' => 'arrow_back', 'size' => 'lg', 'color' => 'black'])
+                @endicon
+
+                Hide
+            </a>
+        </div>
+    @endif
+
+    @include ('Sidebar.item', array('items' => $items, 'appendID' => uniqid(), 'top_level' => true))
+</nav>
+
+<div class="{{$baseClass}}__backdrop" js-sidebar-trigger>
+</div>
