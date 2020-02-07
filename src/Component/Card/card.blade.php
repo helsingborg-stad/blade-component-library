ab6d0f4a4a9e202e746b54d42d8b8eb9d8fa8c2e
Alexander Boman Skoug <alexanderbomanskoug@gmail.com>
2020-02-07 13:17:24 +0100

    This branch contains major design rework, new layouts and containers, new sidebrar component and minor fixes over all
     ## Footer
     - remove top navigation
     - add sitemap
    
     ## Cards
     - remove paper as it messed with flexability
    
     ## Segement
     - remove default heading and body values
    
     ## Button
     - add link function to button
    
     ## Navbar
     - add home link on logo
    
     ## Sidebar
     - add home link on logo

--- src/Component/Card/card.blade.php
+++ src/Component/Card/card.blade.php
@@ -1,58 +1,50 @@
 <!-- card.blade.php -->
 <div id="{{ $id }}" class="{{$class}}" {!! $attribute !!}>
+    @if($top)
+        <div class="{{$baseClass}}__top">{{ $top }}</div>
+    @endif
 
-    @paper([
-        'classList' => [$baseClass.'__paper']
-    ])
+    @if($title['position'] === 'top' && $title['text'])
+        @include('Card.sub.card-top')
+    @endif
 
-        @if($top)
-            <div class="{{$baseClass}}__top">{{ $top }}</div>
-        @endif
+    @if($image)
+        @include('Card.sub.card-image')
+    @endif
 
-        @if($title['position'] === 'top' && $title['text'])
-            @include('Card.sub.card-top')
-        @endif
+    @if($showBody)
+        <div class="{{$baseClass}}__body">
+            @if($slot !== "")
+            {{ $slot }}
+            @endif
+            <!-- BODY -->
+            @include('Card.sub.card-body')
+            <!-- /BODY -->
+        </div>
 
-        @if($image)
-            @include('Card.sub.card-image')
-        @endif
+    @endif
 
-        @if($showBody)
-            <div class="{{$baseClass}}__body">
-                @if($slot !== "")
-                {{ $slot }}
-                @endif
-                <!-- BODY -->
-                @include('Card.sub.card-body')
-                <!-- /BODY -->
-            </div>
+    @if($showFooter)
+        <div class="{{$baseClass}}__bottom @if($dropdown && $dropdown['position'] === 'bottom')
+            {{$baseClass}}__with-bottom-dropdown @endif">
 
-        @endif
+            @if($buttons)
+                @include('Card.sub.card-buttons')
+            @endif
 
-        @if($showFooter)
-            <div class="{{$baseClass}}__bottom @if($dropdown && $dropdown['position'] === 'bottom')
-                {{$baseClass}}__with-bottom-dropdown @endif">
+            @if($icons)
+                @include('Card.sub.card-icons')
 
-                @if($buttons)
-                    @include('Card.sub.card-buttons')
-                @endif
+            @endif
 
-                @if($icons)
-                    @include('Card.sub.card-icons')
+            @if($dropdown && $dropdown['position'] === 'bottom')
+                @include('Card.sub.card-dropdown')
+            @endif
 
-                @endif
+            @if($bottom)
+                {{ $bottom }}
+            @endif
 
-                @if($dropdown && $dropdown['position'] === 'bottom')
-                    @include('Card.sub.card-dropdown')
-                @endif
-
-                @if($bottom)
-                    {{ $bottom }}
-                @endif
-
-            </div>
-        @endif
-
-    @endpaper
-
+        </div>
+    @endif
 </div>
