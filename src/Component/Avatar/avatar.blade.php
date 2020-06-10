36eeda2990c016cae9bc4c7c505e69a777ac4573
Alexander Boman Skoug <39676080+alexanderbomanskoug2@users.noreply.github.com>
2020-06-10 12:09:05 +0200

    fixed intitals centering (#115)

--- src/Component/Avatar/avatar.blade.php
+++ src/Component/Avatar/avatar.blade.php
@@ -18,7 +18,7 @@
 
     @if($initials)
         <svg class="{{$baseClass}}__initials" aria-label="{{ $label }}" viewBox="0 0 1000 1000" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
-            <text font-size="380" y="50%" x="50%" fill="#fff" text-anchor="middle" alignment-baseline="central">{{$initials}}</text>
+            <text font-size="380" y="50%" x="50%" fill="#fff" dominant-baseline="middle" text-anchor="middle" alignment-baseline="central">{{$initials}}</text>
         </svg>
     @endif
 </div>
