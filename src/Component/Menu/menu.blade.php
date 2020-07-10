43d5b6af952ac2adb19bfe299ca682ebbb86cfec
Sebastian Thulin <sebastianthulin@users.noreply.github.com>
2020-07-10 14:05:14 +0200

    Feature/navigation state (#120)
    * json
    
    * Add state data to sidebar item
    
    * Add state data to menu item
    
    * Add state data to navbar
    
    * Revert "json"
    
    This reverts commit 43ae21af9f33730b715a3ee43487183b22f3b8cc.
    
    Co-authored-by: Sebastian Thulin <sebastian.thulin@helsingborg.se>

--- src/Component/Menu/menu.blade.php
+++ src/Component/Menu/menu.blade.php
@@ -5,9 +5,15 @@
   @endif
     <ul class="{{ $baseClass }}__list">
       @foreach($items as $item)
-        <li class="{{$baseClass}}__item {{$baseClass}}__item-{{ $loop->index }} {{ isset($item['active']) ? $baseClass . $activeClass : '' }}">
-          <a href="{{ $item['href'] }}" aria-label="{{ $item['label'] }}" class="{{$baseClass}}__link ripple ripple--before">
-            
+        <li class="{{$baseClass}}__item {{$baseClass}}__item-{{ $loop->index }} {{ $item['active'] ? $baseClass . $activeClass : '' }}">
+          <a  class="{{$baseClass}}__link ripple ripple--before" 
+              href="{{$item['href']}}" 
+              aria-label="{{$item['label']}}" 
+              item-ancestor="{{$item['ancestor'] ? "true" : "false"}}" 
+              item-active="{{$item['active'] ? "true" : "false"}}"
+              item-has-children="{{$item['children'] ? "true" : "false"}}"
+              item-children-async="{{is_array($item['children']) ? "false" : "true"}}"
+          >
             @if(isset($item['icon']) && !empty($item['icon'])) 
               @icon(['icon' => $item['icon']])
               @endicon
