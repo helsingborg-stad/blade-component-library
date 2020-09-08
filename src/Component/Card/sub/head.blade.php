dfded848ad0665f5a815b4f7c99694279913cfd6
Sebastian Thulin <sebastian.thulin@helsingborg.se>
2020-09-08 16:50:10 +0200

    Removed dropdown

--- src/Component/Card/sub/head.blade.php
+++ src/Component/Card/sub/head.blade.php
@@ -15,8 +15,8 @@
     </div>
 @endif
 @if($image)
-<div class="{{$baseClass}}__image {{$baseClass}}__image--{{$image['backgroundColor']}}">
-    <div class="{{$baseClass}}__image-background {{$paddedImage}}" style="background-image:url('{{$image['src']}}');"></div>
+    <div class="{{$baseClass}}__image {{$baseClass}}__image--{{$image['backgroundColor']}}">
+        <div class="{{$baseClass}}__image-background {{$paddedImage}}" style="background-image:url('{{$image['src']}}');"></div>
     </div>
 @endif
 
