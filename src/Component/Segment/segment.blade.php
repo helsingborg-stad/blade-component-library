91c98e84f3cefa3122c87c61057324aac65b052d
Alexander Boman Skoug <alexanderbomanskoug@gmail.com>
2020-09-08 13:45:34 +0200

    Added video background

--- src/Component/Segment/segment.blade.php
+++ src/Component/Segment/segment.blade.php
@@ -1,5 +1,12 @@
 <!-- segment.blade.php -->
 <section id="{{ $id }}" class="{{ $class }}" {!! $attribute !!}>
+
+    @if ($background_video)
+        <video autoplay muted loop id="myVideo" class="{{ $baseClass }}__background--video">
+            <source src="{{ $background_video }}" type="video/mp4">
+        </video>
+    @endif
+
     @if (!empty($top) || !empty($title) || !empty($sub_title))
         @include('Segment.sub.top')
     @endif
