519868bf828f5e175f1583fce6de64e98e3e6051
Sebastian Thulin <sebastian.thulin@helsingborg.se>
2020-09-10 14:25:30 +0200

    Fills sidebar state from backend to enable toggling expansion button and expand subcontainers correctly.

--- src/Component/Sidebar/item.blade.php
+++ src/Component/Sidebar/item.blade.php
@@ -1,7 +1,7 @@
 @if ($top_level)
     <div class="{{$baseClass}}__subcontainer--top">
 @else
-    <div class="{{$baseClass}}__subcontainer"
+    <div class="{{$baseClass}}__subcontainer {{ $isExpanded ? $baseClass . '__item--is-expanded' : '' }}"
         js-toggle-item="{{$appendID}}"
         js-toggle-class="{{$baseClass}}__item--is-expanded">
 @endif
@@ -27,13 +27,21 @@
                 <div class="{{$baseClass}}__toggle"
                     js-toggle-trigger="{{$rndId}}"
                     aria-label="{{$item['id']}}"
-                    aria-pressed="false">
+                    aria-pressed="{{ ( boolval($item['active']) || boolval($item['ancestor']) ) ? 'true' : 'false' }}">
 
                     <div class="bar"></div>
                     <div class="bar"></div>
                 </div>
 
-                @include ('Sidebar.item', array('items' => $item['children'], 'appendID' => $rndId, 'top_level' => false))
+                @include (
+                    'Sidebar.item', 
+                    array(
+                        'items' => $item['children'], 
+                        'appendID' => $rndId, 
+                        'top_level' => false,
+                        'isExpanded' => ( boolval($item['active']) || boolval($item['ancestor']) ) ? true : false 
+                    )
+                )
                 
             @endif
         </div>
