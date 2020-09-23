c41ce50b3b2c55cb0bf50b0c13606b71ac16487c
Alexander Boman Skoug <alexanderbomanskoug@gmail.com>
2020-09-23 13:02:15 +0200

    Prevented empty containers in card components

--- src/Component/Card/card.blade.php
+++ src/Component/Card/card.blade.php
@@ -1,6 +1,12 @@
 <!-- card.blade.php -->
 <{{ $componentElement }} href="{{ $link }}" id="{{ $id }}" class="{{$class}}" {!! $attribute !!}>
     @include('Card.sub.head')
-    @include('Card.sub.body')
-    @include('Card.sub.footer')
+
+    @if ($content || $slot)
+        @include('Card.sub.body')
+    @endif
+    
+    @if ($tags || $buttons)
+        @include('Card.sub.footer')
+    @endif
 </{{ $componentElement }}>
