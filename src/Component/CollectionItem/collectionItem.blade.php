5a51e7424c84ec3b92f9f71112a9d92354a967b4
Sebastian Thulin <sebastian.thulin@helsingborg.se>
2020-10-01 13:34:17 +0200

    Remove comment

--- src/Component/CollectionItem/collectionItem.blade.php
+++ src/Component/CollectionItem/collectionItem.blade.php
@@ -1,8 +1,8 @@
 <!-- collectionItem.blade.php -->
-<{{$elementType}} id="{{ $id }}" class="{{ $class }}" {!! $attribute !!}> AKTIV(CURRENT) / OPEN 
+<{{$elementType}} id="{{ $id }}" class="{{ $class }}" {!! $attribute !!}>
     {!! $slot !!}
     @if($subItemExists)
-        @collection('subCollection' => true)
+        @collection(['subCollection' => true])
             {!! $subItem !!}
         @endcollection
     @endif
