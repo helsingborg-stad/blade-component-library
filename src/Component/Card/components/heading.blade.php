5bf53aad3e9549ed0c7fb7125df09fadddf78a5c
Alexander Boman Skoug <alexanderbomanskoug@gmail.com>
2020-10-07 10:37:13 +0200

    Added underline on card component link

--- src/Component/Card/components/heading.blade.php
+++ src/Component/Card/components/heading.blade.php
@@ -1,3 +1,9 @@
-@typography(['element' => 'h2', 'variant' => 'h2'])
+@typography([
+    'element'   => 'h2',
+    'variant'   => 'h2',
+    'classList' => [
+        $baseClass."__heading"
+    ]
+])
     {!! $heading !!}
 @endtypography
