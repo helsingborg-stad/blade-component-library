aab56b9d63ae09bf90c3ce9445f9b083ccdb1f8d
Sebastian Thulin <sebastian.thulin@helsingborg.se>
2020-10-12 15:04:21 +0200

    Adds badge style

--- src/Component/Date/Date.php
+++ src/Component/Date/Date.php
@@ -13,18 +13,72 @@ class Date extends \BladeComponentLibrary\Component\BaseController
         //Extract array for eazy access (fetch only)
         extract($this->data);
 
+        //Strto time timestamp
+        if(is_array($timestamp) && !empty($timestamp)) {
+            foreach($timestamp as &$item) {
+                $item = strtotime($item);
+            }
+        } else {
+            $timestamp = strtotime($timestamp);
+        }
+
+        //Always get a html timeformat
+        $this->data['htmlDate'] = $this->formatDate($timestamp, $format);
+
+        //Get the format needed
         if ($action == "formatDate") {
-            $this->data['refinedDate'] = $this->formatDate(strtotime($timestamp));
+            $this->data['refinedDate'] = $this->formatDate($timestamp);
         } else if ($action == "timesince") {
-            $this->data['refinedDate'] = $this->convertToHumanReadableUnit(strtotime($timestamp), true);
+            $this->data['refinedDate'] = $this->convertToHumanReadableUnit($timestamp, true);
         } else if ($action == "timeuntil") {
-            $this->data['refinedDate'] = $this->convertToHumanReadableUnit(strtotime($timestamp));
+            $this->data['refinedDate'] = $this->convertToHumanReadableUnit($timestamp);
+        }
+
+        //Badge style data
+        if($badge) {
+
+            $this->data['classList'][] = $this->getBaseClass() . "--badge"; 
+
+            //Get day number
+            foreach(['Month' => 'M', 'Day' => 'j'] as $key => $dformat) {
+                if(is_array($timestamp)) {
+                    if(isset($timestamp[0])) {
+                        $this->data['start' . $key ] = $this->formatDate($timestamp[0], $dformat); 
+                    } else {
+                        $this->data['start '. $key ] = false; 
+                    }
+
+                    if(isset($timestamp[1])) {
+                        $this->data['end' . $key ] = $this->formatDate($timestamp[1], $dformat); 
+                    } else {
+                        $this->data['end' . $key ] = false; 
+                    }
+                } else {
+                    $this->data['start'  . $key ] = $this->formatDate($timestamp, $dformat); 
+                    $this->data['end' . $key ] = false; 
+                }
+            }
+
+            $this->data['day'] = array_unique(array_filter([
+                $this->data['startDay'],
+                $this->data['endDay']
+            ]));
+
+            $this->data['month'] = array_unique(array_filter([
+                $this->data['startMonth'],
+                $this->data['endMonth']
+            ])); 
+
         }
     }
 
-    private function formatDate($timestamp) {
-        $format = 'D d M Y';
-        return date ($format, $timestamp );
+    private function formatDate($timestamp, $format = 'D d M Y') {
+
+        if(is_array($timestamp)) {
+            $timestamp = array_pop($timestamp); 
+        }
+
+        return date($format, $timestamp);
     }
 
     /**
--- src/Component/Date/date.blade.php
+++ src/Component/Date/date.blade.php
@@ -1,1 +1,25 @@
-{{ $refinedDate }}
+<!-- date.blade.php -->
+@if(!$badge) 
+  <!-- Simple Badge -->
+  <time id="{{ $id }}" class="{{$class}}" {!! $attribute !!} datetime="{{ $htmlDate }}">{{ $refinedDate }}</time>
+@endif
+
+@if($badge) 
+
+  <time id="{{ $id }}" class="{{$class}}" {!! $attribute !!} datetime="{{ $htmlDate }}" >
+
+    @if($day)
+      <span class="{{$baseClass}}__month">
+        {{ implode(" - ", $day) }}
+      </span>
+    @endif
+
+    @if($month)
+      <span class="{{$baseClass}}__month">
+        {{ implode(" - ", $month) }}
+      </span>
+    @endif
+
+  </time>
+
+@endif
