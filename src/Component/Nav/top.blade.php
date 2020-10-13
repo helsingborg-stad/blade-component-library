2f2ea11b28ddd13f983e60cfdce199596112ca7c
Sebastian Thulin <sebastian.thulin@helsingborg.se>
2020-10-13 12:39:15 +0200

    Remove top view from nav

--- src/Component/Nav/top.blade.php
+++
@@ -1,9 +0,0 @@
-<ul class="{{$baseClass}}__subcontainer--top">
-    @foreach ($top_items as $item)
-    <li class="{{$baseClass}}__item">
-        <a class="{{$baseClass}}__link" href="{{$item['href']}}" aria-label="{{$item['label']}}" item-active="{{isset($item['active']) ? "true" : "false"}}"  {{ $item['attributeList'] }}>
-            {{$item['label']}}
-        </a>
-    </li>
-    @endforeach
-</ul>
