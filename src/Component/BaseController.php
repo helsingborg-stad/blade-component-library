0f009d7a47cf1e79be0e30bc95d50665e9b21609
Nikolas Ramstedt <nikolas.ramstedt@gmail.com>
2020-11-05 14:38:25 +0100

    Fix: Formatting

--- src/Component/BaseController.php
+++ src/Component/BaseController.php
@@ -21,16 +21,16 @@ class BaseController
      * Unique id
      * @var array
      */
-    private $uid = null; 
+    private $uid = null;
 
     /**
      * Run init
      */
     public function __construct($data)
     {
-        //Load input data 
-        if(!is_null($data) && is_array($data)) {
-            $this->data = array_merge($this->data, $data); 
+        //Load input data
+        if (!is_null($data) && is_array($data)) {
+            $this->data = array_merge($this->data, $data);
         }
 
         //Run
@@ -39,16 +39,16 @@ class BaseController
 
     /**
      * Returns the data
-     * 
+     *
      * @return array Data
      */
     public function getData()
     {
         //Store locally
-        $data = $this->data; 
+        $data = $this->data;
 
         //Generate classes string
-        $data['class'] = $this->getClass(); 
+        $data['class'] = $this->getClass();
         $data['baseClass'] = $this->getBaseClass();
 
         //Create attibute string
@@ -62,20 +62,20 @@ class BaseController
         $data['slotHasData'] = !empty($this->accessProtected($this->data['slot'], "html"));
 
         //Applies a general wp filter
-        if(function_exists('apply_filters')) {
+        if (function_exists('apply_filters')) {
             $data = apply_filters("BladeComponentLibrary/Component/Data", $data);
         }
 
         //Applies a general wp filter
-        if(function_exists('apply_filters')) {
+        if (function_exists('apply_filters')) {
             $data = apply_filters($this->createFilterName($this) . DIRECTORY_SEPARATOR . "Data", $data);
         }
 
         //Applies single filter for each data item (class and data exepted)
-        if(function_exists('apply_filters')) {
-            if(is_array($data) && !empty($data)) {
-                foreach($data as $key => $item) {
-                    if(!in_array($key, array("data", "classes"))) {
+        if (function_exists('apply_filters')) {
+            if (is_array($data) && !empty($data)) {
+                foreach ($data as $key => $item) {
+                    if (!in_array($key, array("data", "classes"))) {
                         $data[$key] = apply_filters($this->createFilterName($this) . DIRECTORY_SEPARATOR . ucfirst($key), $data[$key]);
                     }
                 }
@@ -88,16 +88,16 @@ class BaseController
 
     /**
      * Returns set or dynamic id
-     * 
+     *
      * @return string Id of the component
      */
     private function getId()
     {
         //Store locally
-        if(isset($this->data['id']) && !empty($this->data['id'])) {
-            $id = (string) $this->data['id']; 
+        if (isset($this->data['id']) && !empty($this->data['id'])) {
+            $id = (string) $this->data['id'];
         } else {
-            $id = ""; 
+            $id = "";
         }
 
         return (string) strtolower($id);
@@ -105,51 +105,49 @@ class BaseController
 
     /**
      * Returns and sets a dynamic id
-     * 
+     *
      * @return string Random id
      */
     public function getUid()
     {
-        if(!is_null($this->uid)) {
-            return $this->uid; 
+        if (!is_null($this->uid)) {
+            return $this->uid;
         }
         return $this->uid = uniqid();
     }
     
     /**
      * Returns the classes
-     * 
+     *
      * @return string Css classes
      */
     private function getClass($implode = true)
     {
         //Store locally
-        if(isset($this->data['classList']) && is_array($this->data['classList'])) {
-            
+        if (isset($this->data['classList']) && is_array($this->data['classList'])) {
             array_unshift(
-                $this->data['classList'], 
+                $this->data['classList'],
                 (string) $this->getBaseClass()
-            ); 
+            );
 
             $class = (array) $this->data['classList'];
-
         } else {
             $class = array();
         }
 
         //Applies a general wp filter
-        if(function_exists('apply_filters')) {
+        if (function_exists('apply_filters')) {
             $class = apply_filters($this->createFilterName($this) . DIRECTORY_SEPARATOR . "Class", $class);
         }
 
         //Applies a general wp filter
-        if(function_exists('apply_filters')) {
+        if (function_exists('apply_filters')) {
             $class = apply_filters("BladeComponentLibrary/Component/Class", $class);
         }
 
         //Return manipulated classes as array
-        if($implode === false) {
-            return (array) $class; 
+        if ($implode === false) {
+            return (array) $class;
         }
 
         //Return manipulated data array as string
@@ -158,57 +156,60 @@ class BaseController
 
     /**
      * Returns the first class assigned, used as base class
-     * 
+     *
      * @return string A single css class
      */
     protected function getBaseClass()
     {
         //If base class is specified from component controller then use that
-        if($this->data['baseClass']) {
+        if ($this->data['baseClass']) {
             return $this->data['baseClass'];
         }
         
         //Get all parts of the location
         $namespaceParts = explode(
-            "\\", 
+            "\\",
             get_called_class()
-        ); 
+        );
 
         //Create string
-        return strtolower("c-" . end($namespaceParts)); 
+        return strtolower("c-" . end($namespaceParts));
     }
 
-    private function getAttribute($implode = true) {
+    private function getAttribute($implode = true)
+    {
 
         //Store locally
-        if(isset($this->data['attributeList']) && is_array($this->data['attributeList'])) {
-            $attribute = (array) $this->data['attributeList']; 
+        if (isset($this->data['attributeList']) && is_array($this->data['attributeList'])) {
+            $attribute = (array) $this->data['attributeList'];
         } else {
             $attribute = array();
         }
 
         //Add unique id
-        $attribute['data-uid'] = $this->getUid(); 
+        $attribute['data-uid'] = $this->getUid();
 
         //Applies a general wp filter
-        if(function_exists('apply_filters')) {
+        if (function_exists('apply_filters')) {
             $attribute = apply_filters($this->createFilterName($this) . DIRECTORY_SEPARATOR . "Attribute", $attribute);
         }
 
         //Applies a general wp filter
-        if(function_exists('apply_filters')) {
+        if (function_exists('apply_filters')) {
             $attribute = apply_filters("BladeComponentLibrary/Component/Attribute", $attribute);
         }
 
         //Return manipulated classes as array
-        if($implode === false) {
-            return (array) $attribute; 
+        if ($implode === false) {
+            return (array) $attribute;
         }
 
         //Return manipulated data array as string
-        return (string) implode(' ', array_map(
-            function ($v, $k) { 
-                    return sprintf('%s="%s"', $k, $v); 
+        return (string) implode(
+            ' ',
+            array_map(
+                function ($v, $k) {
+                    return sprintf('%s="%s"', $k, $v);
                 },
                 array_values($attribute),
                 array_keys($attribute)
@@ -218,25 +219,25 @@ class BaseController
 
     /**
      * Creates a filter name
-     * 
+     *
      * @return string
      */
-    private function createFilterName($class) 
+    private function createFilterName($class)
     {
         //Get all parts of the location
         $name = explode(
-            "\\", 
+            "\\",
             get_class($class)
-        ); 
+        );
 
         //Remove duplicates
-        $name = array_unique($name); 
+        $name = array_unique($name);
 
         //Create string
-        return implode(DIRECTORY_SEPARATOR, $name); 
+        return implode(DIRECTORY_SEPARATOR, $name);
     }
 
-    /** 
+    /**
      * Proxy for accessing private props
      *
      * @return string Array of values
