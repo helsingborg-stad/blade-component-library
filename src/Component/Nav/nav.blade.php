de0efb4b669724793fcb835d94917e3eeea726f6
Nikolas Ramstedt <nikolas.ramstedt@gmail.com>
2020-11-06 15:01:55 +0100

    Implement nested nav & clean up

--- src/Component/Nav/nav.blade.php
+++ src/Component/Nav/nav.blade.php
@@ -1,11 +1,42 @@
 <!-- nav.blade.php -->
-<nav id="{{ $id }}" class="{{$class}}" {!! $attribute !!}>
-    @include(
-        'Nav.item', 
-        array(
-            'items' => $items, 
-            'appendID' => uniqid(),
-            'topLevel' => true
-        )
-    )
-</nav>
+@if ($items) 
+    <ul id="{{ $id }}" class="{{$class}} unlist" {!! $attribute !!}>
+        @foreach ($items as $item)
+            <li 
+                class="{{$baseClass}}__item {{$item['active'] ? 'is-current' : ''}} {{$item['children'] & $item['active'] ? 'is-open has-fetched' : ''}}"
+        
+                {{-- Append dynamic attributes --}}
+                {!! !empty($item['attributeList']) ? $buildAttributes($item['attributeList']) : '' !!}
+            >
+                
+                <a  class="{{$baseClass}}__link" 
+                    href="{{$item['href']}}" 
+                    aria-label="{{$item['label']}}" 
+                >
+                    {{$item['label']}}
+                </a>
+
+                @if ($item['children'])
+                    @if($includeToggle)
+                        @button([
+                            'classList' => [ $baseClass . '__toggle', 'js-toggle-children'],
+                            'style' => 'basic',
+                            'icon' => 'expand_more',
+                            'size' => 'lg',
+                            'pressed' =>  $item['active'] ? 'true' : 'false',
+                        ])
+                        @endbutton
+                    @endif
+
+                    @if(is_array($item['children'])) 
+                        @nav([
+                            'items' => $item['children'],
+                            'isExpanded' => (boolval($item['active']) || boolval($item['ancestor']) ) ? true : false 
+                        ])
+                        @endnav
+                    @endif
+                @endif
+            </li>
+        @endforeach
+    </ul>
+@endif
