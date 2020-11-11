677a69d737acf83a894e39d669165534cf1fd112
Nikolas Ramstedt <nikolas.ramstedt@helsingborg.se>
2020-11-11 16:03:55 +0100

    Feature/nav (#157)
    * Fix: Formatting
    
    * Lift out build method for html attributes to own method & make avalible
    
    * Remove old partial
    
    * Implement nested nav & clean up
    
    * Fix: item states
    
    * unlist breadcrumb
    
    * Move unlist to controller
    
    Co-authored-by: Sebastian Thulin <sebastian.thulin@helsingborg.se>

--- src/Component/Nav/nav.blade.php
+++ src/Component/Nav/nav.blade.php
@@ -1,11 +1,45 @@
 <!-- nav.blade.php -->
-<nav id="{{ $id }}" class="{{$class}}" {!! $attribute !!}>
-    @include(
-        'Nav.item', 
-        array(
-            'items' => $items, 
-            'appendID' => uniqid(),
-            'topLevel' => true
-        )
-    )
-</nav>
+@if ($items) 
+    <ul id="{{ $id }}" class="{{$class}}" {!! $attribute !!}>
+        @foreach ($items as $item)
+            <li 
+                class="{{$baseClass}}__item {{$item['active'] ? 'is-current' : ''}}{{$item['active'] && $item['children'] || $item['ancestor'] ? ' is-open has-fetched' : ''}}"
+        
+                {{-- Append dynamic attributes --}}
+                {!! !empty($item['attributeList']) ? $buildAttributes($item['attributeList']) : '' !!}
+            >
+                
+                <a  class="{{$baseClass}}__link" 
+                    href="{{$item['href']}}" 
+                    aria-label="{{$item['label']}}" 
+                >
+                    {{$item['label']}}
+                </a>
+
+                @if ($item['children'])
+                    @if($includeToggle)
+                        @button([
+                            'classList' => [ $baseClass . '__toggle', 'js-toggle-children'],
+                            'style' => 'basic',
+                            'icon' => 'expand_more',
+                            'size' => 'lg',
+                            'pressed' =>  $item['active'] ? 'true' : 'false',
+                        ])
+                            @loader(['size' => 'sm'])
+                            @endloader
+                        @endbutton
+                    @endif
+
+                    @if(is_array($item['children'])) 
+                        @nav([
+                            'items' => $item['children'],
+                            'isExpanded' => (boolval($item['active']) || boolval($item['ancestor']) ) ? true : false,
+                            'includeToggle' => $includeToggle
+                        ])
+                        @endnav
+                    @endif
+                @endif
+            </li>
+        @endforeach
+    </ul>
+@endif
