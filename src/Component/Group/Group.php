228c06de12a50a0d4177b5e7d5f0e7f13255b6b9
Joel Bernerman <joel.bernerman@helsingborg.se>
2020-11-11 15:11:53 +0100

    Some code sniffer error fixes

--- src/Component/Group/Group.php
+++ src/Component/Group/Group.php
@@ -8,17 +8,15 @@ namespace BladeComponentLibrary\Component\Group;
  */
 class Group extends \BladeComponentLibrary\Component\BaseController
 {
-	public function init()
-	{
-		//Extract array for eazy access (fetch only)
-		extract($this->data);
+    public function init()
+    {
+        //Extract array for eazy access (fetch only)
+        extract($this->data);
 
-		if($direction == "vertical") {
-			$this->data['classList'][] = $this->getBaseClass() . "--vertical"; 
-		} else {
-			$this->data['classList'][] = $this->getBaseClass() . "--horizontal"; 
-		}
-
-	}
-
+        if ($direction == "vertical") {
+            $this->data['classList'][] = $this->getBaseClass() . "--vertical";
+        } else {
+            $this->data['classList'][] = $this->getBaseClass() . "--horizontal";
+        }
+    }
 }
