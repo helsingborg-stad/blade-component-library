6eb43e939f28aa955e112e63ad0047efad3e074a
Johan Silvergrund <johan.silvergrund@hiq.se>
2020-11-18 14:00:13 +0100

    Allowed html chars in output (#159)

---
+++ vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+    ),
+    'reference' => 'da58c94a352c82469c1535ede10ed52669ffab60',
+    'name' => 'helsingborg-stad/blade-component-library',
+  ),
+  'versions' => 
+  array (
+    'helsingborg-stad/blade-component-library' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'da58c94a352c82469c1535ede10ed52669ffab60',
+    ),
+  ),
+);
