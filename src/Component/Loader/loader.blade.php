b37a61a90784249d52e628a7eb5a22eb0787b145
Alexander Boman Skoug <alexanderbomanskoug@gmail.com>
2021-01-18 16:30:30 +0100

    Added labeledby, role alert to notify that new content is being loaded

--- src/Component/Loader/loader.blade.php
+++ src/Component/Loader/loader.blade.php
@@ -1,9 +1,10 @@
 <!-- loader.blade.php -->
 
-<{{$componentElement}} id="{{ $id }}" class="{{ $class }}" {!! $attribute !!}>
+<{{$componentElement}} id="{{ $id }}" class="{{ $class }}" {!! $attribute !!} role="alert" aria-busy="true" @if($text) aria-labeledby="loader__label__{{$id}}" @endif>
 </{{$componentElement}}>
 @if ($text)
     @typography([
+        "id" => "loader__label__".$id,
         "variant" => "meta",
         "element" => "h5",
         "classList" => ['c-loader--text', 'u-display--none']
