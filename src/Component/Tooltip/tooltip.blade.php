849fc38275c7dcb4203425d237ae0c8e884ced05
Alexander Boman Skoug <alexanderbomanskoug@gmail.com>
2021-01-18 16:36:46 +0100

    added role to tooltip, the tooltip is in the aria-label as we don't follow traditional semantics for tooltips and the aria attributes

--- src/Component/Tooltip/tooltip.blade.php
+++ src/Component/Tooltip/tooltip.blade.php
@@ -5,6 +5,7 @@
     id="{{ $id }}" 
     class="{{ $class }} {{ 'c-tooltip__'.$placement }}"
     js-bind-hover="tooltip"
-    data-title="{{ $title }}" {!! $attribute !!}>
+    data-title="{{ $title }}" {!! $attribute !!}
+    role="tooltip" aria-label="Tooltip: {{ $title }}">
     {{$beforeContent}} {{ $slot }} {{$afterContent}}
 </{{ $componentElement }}>
