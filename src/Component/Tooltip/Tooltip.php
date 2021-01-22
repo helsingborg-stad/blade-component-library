4011e28bf7de9d96f07576c65e31821026ef2c05
Sebastian Thulin <sebastian.thulin@helsingborg.se>
2021-01-22 11:03:19 +0100

    Move attributes in tooltip to controller.

--- src/Component/Tooltip/Tooltip.php
+++ src/Component/Tooltip/Tooltip.php
@@ -7,7 +7,23 @@ class Tooltip extends \BladeComponentLibrary\Component\BaseController
     public function init() {
         //Extract array for eazy access (fetch only)
         extract($this->data);
+
+        //Remove html
+        $title = strip_tags($title); 
         
+        //Is link? 
         $this->data['isLink'] = $this->data['componentElement'] === 'a';
+
+        //Add classes
+        $this->data['classList'][] = $this->getBaseClass() . '__'.$placement; 
+
+        //Adds attributes
+        $this->data['attributeList']['js-bind-hover'] = 'tooltip'; 
+        $this->data['attributeList']['data-title'] = $title; 
+        $this->data['attributeList']['role'] = 'tooltip'; 
+        
+        if($title) {
+            $this->data['attributeList']['aria-label'] = 'Tooltip: ' . $title; 
+        }
     }
 }
