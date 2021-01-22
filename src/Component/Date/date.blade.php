c47e4317c5a819b29a1c9672e6de5e0e0ce09347
Sebastian Thulin <sebastian.thulin@helsingborg.se>
2021-01-22 15:51:32 +0100

    Indentation fix.

--- src/Component/Date/date.blade.php
+++ src/Component/Date/date.blade.php
@@ -1,11 +1,10 @@
 @if($tooltipDate)
-@tooltip(['componentElement' => 'span', 'placement' => 'top'])
-  @slot('title')
-      {{ $tooltipDate }}
-  @endslot
-  <time class="{{ $metaDate }}">{{ $refinedDate }}</time>
-@endtooltip
-
+  @tooltip(['componentElement' => 'span', 'placement' => 'top'])
+    @slot('title')
+        {{ $tooltipDate }}
+    @endslot
+    <time class="{{ $metaDate }}">{{ $refinedDate }}</time>
+  @endtooltip
 @else
   <time class="{{ $metaDate }}">{{ $refinedDate }}</time>
 @endif
