a75466660e542907ce706683a31f978963d47757
Sebastian Thulin <sebastian.thulin@helsingborg.se>
2021-01-22 11:12:16 +0100

    Move attributes to controller.

--- src/Component/Textarea/textarea.blade.php
+++ src/Component/Textarea/textarea.blade.php
@@ -1,17 +1,6 @@
 <!-- textarea.blade.php -->
 <div class="{{$class}}" role="textarea">
-    <textarea {!! $attribute !!}
-            aria-multiline="true"
-              placeholder="{{$label}}"
-              aria-placebolder="{{$label}}"
-              aria-label="{{$label}}"
-              id="{{ $id }}"
-              @if($required)
-                  required
-                  data-required="1"
-                  aria-required="true"
-              @endif
-    >{{$value}}</textarea>
+    <textarea id={{$id}} name="{{$name}}"{!! $attribute !!}>{{$value}}</textarea>
     <label class="c-textarea--label">{{$label}}</label>
     <div id="error_{{ $id }}_message" class="c-textarea-invalid-message">
         @icon([
